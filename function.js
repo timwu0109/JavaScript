5e86bbc392a642bd69b41eeca761d7315eba135c
TimWu <mr.oldfive@gmail.com>
2023-03-29 02:01:35 +0800

    day2 funtction argument and parameter

---
+++ function.js
@@ -0,0 +1,19 @@
+// function tim(x) {
+//   for(let a = x ; a > 0 ; a--){
+//     console.log('tim'.repeat(a));
+//   }
+// };
+
+// tim(10)
+
+// 參數parameter >
+// argument 多於 parameter 會當沒看見， 
+// argiment 少於 parameter 則會自動補 undefind
+
+// function intergeText( a , b, c ){
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+// };
+
+// intergeText(1,2,3,4) < 引數argument
