ab8600da1f73503b75bf852ed9896cd3a16afe5b
TimWu <mr.oldfive@gmail.com>
2023-04-03 16:01:44 +0800

    User DOM change HTML value

--- dom.js
+++ dom.js
@@ -8,8 +8,23 @@
 
 
 // 以下兩中方式都可以一一把抓一樣的 DOM元素
-const ulDoms1 = document.getElementsByClassName('list')
-console.log(ulDoms1);
 
-const ulDoms = document.querySelectorAll('.list')
-console.log(ulDoms);
+// const ulDoms1 = document.getElementsByClassName('list')
+// console.log(ulDoms1);
+
+// for(let i = 0 ; i < ulDoms1.length;i++ ){
+//   console.log(ulDoms1[i]);
+// }
+
+// const ulDoms = document.querySelectorAll('.list')
+// console.log(ulDoms);
+
+
+//如何取出第二值，當抓到一樣東西的時漏瀏覽器會把他們變成陣列，所以就可以用陣列的index去抓出來 
+// console.log(ulDoms1[1]);
+
+
+// 抓到DOM元素後如何改變HTML的文組書出
+const changeDoms1 = document.getElementById('name')
+
+changeDoms1.textContent = "我抓到DOM Value 改變你了"
