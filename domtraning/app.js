e9292fa348c1625ef8e1bbfac4af07f987501260
TimWu <mr.oldfive@gmail.com>
2023-04-07 02:11:55 +0800

    Use CSS picker catch last element and Use father removeChild

--- domtraning/app.js
+++ domtraning/app.js
@@ -13,23 +13,29 @@
 
 
 // 練習題第二題 刪除最後一個，然後再新增元素
+// 用CSS 選取器抓到最後一個li li:last-child
+// appendChild 是在子層的最下面增加element
 
 const removeBtn = document.querySelector('#removeBtn')
 const addBtn = document.querySelector('#addBtn')
+const container = document.querySelector('ul')
 
 
 removeBtn.addEventListener('click' , ()=>{
   const lastChild = document.querySelector('ul> li:last-child')
-  
+
+  // 在這邊lastChild 如果被刪除完，就會變成null 而null = false 所以就不會做
+  // if 裡面兩種方式去看掉el ，一種事直接把自己remove，另外一種是教父層用removeChild去砍子層
   if(lastChild){
-    lastChild.remove()
+    container.removeChild(lastChild)
+    // lastChild.remove()
   }
   
 })
 
 addBtn.addEventListener('click' , ()=>{
-  const container = document.querySelector('ul')
   const el = document.createElement('li')
+
   el.textContent = 456
   container.appendChild(el)
 })
