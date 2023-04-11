132c35323aae3b96a637b043f3e41eb6782cfd0e
TimWu <mr.oldfive@gmail.com>
2023-04-12 00:58:25 +0800

    about the API verb and What is AJAX do ?

---
+++ API.js
@@ -0,0 +1,10 @@
+// API application programming interface
+
+// API 就是一個網址，資料來源
+// API 本質是個介面，去某個地方取得資料
+
+// 取得資料的動詞 GET取資料/POST寫入資料/PUT修改資料/PATCH修改資料/DELETE刪除資料，我可以對那個介面做的動作，符合以上動作就是RESTful API 他是一種設計風格
+
+// 資料傳輸格式 XML/JSON/CSV
+
+// AJAX Asynchronous JavaScript amd XML , 非同步處理，他是一種概念，不是技術也不是語言。要如何理解非同步，就想像你在點購物商品得時候跑出動畫顯示轉圈的時候，後面其實在跟要資料！另外一個比較具代表的就是，google map 地圖不可能事先載入全部，會導致流量太大所以當拖移的時候，就會先去載入範圍外面的地圖並且拼接起來，不讓畫面曝光這就是AJAX
