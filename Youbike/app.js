a9c7444491fb23691bfe2fe90f11b5ced2467c06
TimWu <mr.oldfive@gmail.com>
2023-04-16 02:14:42 +0800

    traning 2

--- Youbike/app.js
+++ Youbike/app.js
@@ -1,6 +1,8 @@
 // 程式碼寫這裡
 //preventDefault取消預設屬性
 //submit 表單事件（只要有送出就會觸發事件）
+// form 還有兩個很重要的功能就是 method:"GET" < 用什麼方法送表單，預設是GET action:"https://google.com" < 送到哪裡如果沒填預設是送回自己
+
 
 //先去網站抓資料吧網址抓回來
 const DATE_SOUCE = "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
