4b7bf99c71b396b81263e7efc2442dbf79112755
TimWu <mr.oldfive@gmail.com>
2023-04-16 02:14:42 +0800

    traning 2

--- Youbike/app2.js
+++ Youbike/app2.js
@@ -1,39 +1,47 @@
-const DATE_SOUCE = "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
-const searchForm = document.querySelector("#searchForm")
-const searchKeywordInput = document.querySelector("#searchKeyword")
-const siteList = document.querySelector("siteList")
-searchForm.addEventListener("submit" , (e) => {
-    e.preventDefault()
-    const keyValue =searchKeyword.value.trim()
+const youBikeAPI = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
+
+const searchForm = document.querySelector('#searchForm')
+const searchKeywordInput = document.querySelector('#searchKeyword')
+const siteList =document.querySelector('.siteList')
 
 
-    
-    if (keyValue !== "")
-        {fetch(DATE_SOUCE)
-            .then((reps) => {
-                return reps.json()
-            })
-            .then((stations)=>{
-                const result = stations
-                .filter((stationInformation) => {
-                    return stationInformation.ar.includes(keyValue)
-                })
-                .reduce((acc , stationInformation) =>{
-                       return acc + (`<li class="list-group-item  fs-5"><iclass="fas fa-bicycle"></iclass=>
-                       ${stationInformation.sna.replace("YouBike2.0_","")}(${stationInformation.sbi}/${stationInformation.tot})<br><small class="text-muted">${stationInformation.ar}</small></li>`
-                    )    
-                } , "")
-                siteList.textContent = ""
-                siteList.insertAdjacentHTML("afterbegin" , result)
-                searchKeywordInput.foucs()
-            })
-            .catch((err)=>{
-                
-                console.log(err);
-            })
-        }else{
-            alert("Please enter adderss thank for cooperate ")
-        }
+searchForm.addEventListener('submit' ,(e)=>{
+  e.preventDefault()
+  const keyWord = searchKeywordInput.value.trim()
+
+  if(keyWord !== ''){
+    fetch(youBikeAPI)
+    .then((resp)=> resp.json())
+    .then((stations)=>{
+      renderStation(stations,keyWord)
+      // 這邊我keyword卡很久，不需要把外部的東西帶到then裡面因為then是關注fetch，所以call function要帶任何參數進去直接找到那function帶進去就好了
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
 })
 
+function filterStation(stations,keyWord){
+  return stations.filter((station)=>station.ar.includes(keyWord))}
 
+function reduceStation(station){
+  return station.reduce((acc , {sna , sbi ,tot , ar})=> acc + `<li class="list-group-item fs-5">
+    <i class="fas fa-bicycle"></i>
+    ${sna.replace("YouBike2.0_" , " ")}(${sbi}/${tot})<br>
+        <small class="text-muted">${ar}</small>
+    </li>` , "")
+}
+
+
+function renderItem (result){
+  siteList.textContent = ''
+  siteList.insertAdjacentHTML('afterbegin' , result)
+}
+
+function renderStation(stations,keyWord) {
+  let result = filterStation(stations,keyWord) 
+  result = reduceStation(result)
+ 
+  renderItem(result)
+}
--- Youbike/app3.js
+++ Youbike/app3.js
@@ -1,41 +1,53 @@
-const DATE_SOUCE = "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
-const searchForm = document.querySelector("#searchForm")
-const siteList = document.querySelector(".siteList")
-const searchKeywordIput =document.querySelector("#searchKeyword")
-searchKeywordIput.focus()
+const DATE_SOUCER = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
 
 
-searchForm.addEventListener("submit",(e) =>{
-    e.preventDefault()
- const ketWordValue =  searchKeywordIput.value.trim()
+const searchForm = document.querySelector('#searchForm')
+const searchKeywordInput = document.querySelector('#searchKeyword') 
+const siteList = document.querySelector('.siteList')
 
-    if(ketWordValue !== "" ){
-        fetch(DATE_SOUCE)
-            .then((resp)=>{
-            return resp.json()
-            })
-            .then((stations)=>{
-                const result = stations
-                .filter((station)=>{
-                    return station.ar.includes(ketWordValue)
-                })// 
-                .reduce((acc , {sna , sbi , tot ,ar} )=>{
-                    return (
-                        acc + `<li class="list-group-item fs-5"><i class="fas fa-bicycle"></i>
-                       ${sna.replace("YouBike2.0_","")}(${sbi}/${tot})<br><small class="text-muted">${ar}</small></li>`
-                    )
-                }, "")
-                siteList.textContent=" "
-                siteList.insertAdjacentHTML("beforeend" , result)
-                searchKeywordIput.focus()
-            })
-            .catch((err)=>{
-                console.log(err);
-            })
-    }else{
-        alert("Please entern message thank for your cooperate")
-    }
+
+searchForm.addEventListener('submit' , (e)=>{
+  e.preventDefault()
+  const keyWord = searchKeyword.value.trim()
+
+  if(keyWord !== ''){
+    fetch(DATE_SOUCER)
+    .then((resp)=> resp.json())
+    .then((stations)=>{
+      renderStation(stations , keyWord)
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }else{
+    alert('請輸入正確文字')
+  }
 })
 
+function filterStation(stations , keyWord) {
+  return stations.filter((station)=>{ return station.ar.includes(keyWord);
+  })
+}
 
+function reduceStation(stations) {
+  return stations.reduce((acc , {sna , sbi , tot ,ar})=>
+    acc + `<li class="list-group-item fs-5">
+    <i class="fas fa-bicycle"></i>
+    ${sna.replace('YouBike2.0_','')}(${sbi}/${tot})<br>
+    <small class="text-muted">${ar}</small>
+    </li>`
+  , '')
+}
+
+function renderItem(result) {
+  siteList.textContent = ''
+  siteList.insertAdjacentHTML('afterbegin' , result)
+}
+
+function renderStation(stations ,keyWord) {
+  let result = filterStation(stations , keyWord)
+  result = reduceStation(result)
+
+  renderItem(result)
+}
 
