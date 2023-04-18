8f074dab18a4fad9343764d1f8b460e60ac9ebc6
TimWu <mr.oldfive@gmail.com>
2023-04-19 00:20:59 +0800

    traning for setTimeout

--- capture.js
+++ capture.js
@@ -1,5 +1,6 @@
 // addEventListener(type, listener, useCapture)
 // 兩個事件相疊加在一起的時候，要把它想成是立體圖一個在上面一個在下面，當我們點擊的時候會有一條Ｕ型的事件流經過，點擊下的時候會往下面探下去，然後再往上回來 下去的時候是useCapture ，向上是Bubbling 
+
 // 當我們點擊得時候會發現下層的會先做，在做上層的是因為addEventListener第三個選項Capture 預設是false設定在Bubbling得時候再會做，所以當我們把上層 div a 把預設的false 改成true的時候就變成上層先做完再換下層了
 
 // e.stopPropagation(); 使用stopPropagation停止傳導就可以防止事件流往下繼續傳播
