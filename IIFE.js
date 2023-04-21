6c04c970bf27989bb517d4c5341993b4f0006896
TimWu <mr.oldfive@gmail.com>
2023-04-21 15:11:59 +0800

    Javascript import export

--- IIFE.js
+++ IIFE.js
@@ -1,7 +1,13 @@
+import {add} from './app'
+import {min} from './app'
 // IIFE = Immediately(立即) Invoked(調用) Function Expression(表達)
 // 也就是立即函式 ，立即函式的好處就任何變數都不會污染到外面來，適合拿來做套件
 
-(function (n) {
-  console.log(n);
-})()
+// (function (n) {
+//   console.log(n);
+// })(123);
+
+add()
+min("tim" , "oli")
+
 
---
+++ app.js
@@ -0,0 +1,8 @@
+export function add(){
+  console.log(123);
+  }
+  
+  
+export function min(a ,b){
+console.log(a,b);
+}
