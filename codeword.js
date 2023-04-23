e5d8aecd1d5d173b131862718206089ba658fc36
TimWu <mr.oldfive@gmail.com>
2023-04-24 02:46:54 +0800

    odeword tarning 2

--- codeword.js
+++ codeword.js
@@ -13,3 +13,30 @@
 
 
 
+//   題目二
+function isIsogram(str){
+  // const a = new Set(str.toUpperCase()).size === str.length
+  // console.log(a);
+  let i, j;
+  str = str.toLowerCase();
+  for(i = 0; i < str.length; ++i)
+    for(j = i + 1; j < str.length; ++j)
+      if(str[i] === str[j]){
+        return false;
+      }
+  return true
+}
+
+console.log(isIsogram("Dermatoglyphics"));
+console.log(isIsogram("isogram"));
+console.log(isIsogram("aba"));
+console.log(isIsogram("moOse"));
+console.log(isIsogram("isIsogram"));
+console.log(isIsogram(""));
+
+isIsogram("Dermatoglyphics") // true 
+isIsogram("isogram") // true
+isIsogram("aba")  //false, "same chars may not be adjacent" 
+isIsogram("moOse")  // false, "same chars may not be same case" 
+isIsogram("isIsogram") // false 
+isIsogram("") // true, "an empty string is a valid isogram" 
